0c9a3a93bc7363268c8844a1abe7f1a8d4508253
Mohammad Ammar <78434776+ammar-stack@users.noreply.github.com>
2024-05-08 23:05:05 -0700

    Create oddOReven.dart

---
+++ oddOReven.dart
@@ -0,0 +1,19 @@
+void main() {
+  //For even numbers
+  for(int i = 1;i<=20;i++){
+    if(i%2==0){
+      print("$i is an even number");
+    }
+  }
+  
+  //space
+  
+  print("\n\n\n");
+  
+  //For odd number
+  for(int j = 1;j<=20;j++){
+    if(j%2!=0){
+      print("$j is an odd number");
+    }
+  }
+}
